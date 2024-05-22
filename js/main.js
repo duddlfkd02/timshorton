108d24f3e6a436a84aeab43b8bec19fdebe093d1
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-05-22 17:37:48 +0900

    header 수정

--- js/main.js
+++ js/main.js
@@ -1,40 +1,56 @@
-$(document).ready(function(){
-    $('nav > ul > li').on('mouseover', function(){
+$(document).ready(function () {
+    $('nav > ul > li').on('mouseover', function () {
         $('.submenu').stop().slideDown(200);
     })
 
-    $('nav > ul > li').on('mouseout', function(){
+    $('nav > ul > li').on('mouseout', function () {
         $('.submenu').stop().slideUp(200);
     })
 
 
-/*----애니메이션 구간----*/
+    /*----애니메이션 구간----*/
 
-    $(document).on("scroll",function(){
-        if($(document).scrollTop() > 62){ 
+    $(document).on("scroll", function () {
+        if ($(document).scrollTop() > 62) {
             $(".content1").addClass("animate__fadeInUp");
-            $(".content1").css({"visibility":"visible"});
+            $(".content1").css({ "visibility": "visible" });
         }
 
-        if($(document).scrollTop() > 637){ 
+        if ($(document).scrollTop() > 637) {
             $(".content2 > div").addClass("animate__fadeInUp");
-            $(".content2 > div").css({"visibility":"visible"})
+            $(".content2 > div").css({ "visibility": "visible" })
         }
 
 
     })
 
-    $('.burger').click(function(){
+    $('.burger').click(function () {
         $(this).toggleClass('active');
         $('nav').slideToggle(500);
     });
 
-    $(window).resize(function(){
+    $(window).resize(function () {
         let wWidth = $(window).width();
 
-        if(wWidth > 699){
+        if (wWidth > 699) {
             $("nav").removeAttr("style");
         }
+    })
+
+
+
+
+    $(window).scroll(function () {
+        let scrollV = $(document).scrollTop();
+
+        if (scrollV > 700) {
+            $('nav').css("background-color", "#c8102f"), $('header').css("background-color", "#ffffff");
+        }
+
+        if (scrollV < 700) {
+            $('nav').css("background-color", "transparent"), $('header').css("background-color", "transparent");
+        }
+
     })
 
 
